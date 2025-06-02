117a4e82726264eecc09e7149047eed20a736acd
Imran Immi <imrnimmi@gmail.com>
2025-06-03 00:25:55 +0600

    update

--- lib/core/screens/note_screen.dart
+++ lib/core/screens/note_screen.dart
@@ -6,11 +6,35 @@ class NoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Write your Notes"), centerTitle: true),
+      appBar: AppBar(
+        title: Text("Write your Notes", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: EdgeInsets.all(25),
         child: Center(
-          child: Column(children: [TextField(decoration: InputDecoration())]),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Description",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
